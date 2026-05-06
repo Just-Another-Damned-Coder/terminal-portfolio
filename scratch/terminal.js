0f13e4c394ceb21c70dc0ef65ef9bb587277cdce
Moris Johnson <luv1tusmaximus@outlook.com>
2026-05-06 21:13:21 +0530

    chore: remove un-necessary files

--- scratch/terminal.js
+++
@@ -1,7 +0,0 @@
-const user_pattern = /username (\w+)/; // Define the pattern as a regex
-let string = "usernmoris";
-
-// Now use the regex to match against the string
-let result = string.match(user_pattern);
-
-console.log(result);
